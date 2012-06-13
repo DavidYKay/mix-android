cb54140563b5f4ccf641ae1dafb3b680be306f1d
David Young-Chan Kay <davidykay@gmail.com>
2012-06-12 23:14:51 -0400

    added most of the opcodes with byte definitions.

--- desktop/src/com/davidykay/mix/model/Opcode.java
+++ desktop/src/com/davidykay/mix/model/Opcode.java
@@ -9,7 +9,7 @@ public class Opcode implements Byteable {
       return new Opcode(type);
     }
   }
-  
+
   public Type type;
 
   public Opcode(Type type) {
@@ -17,9 +17,58 @@ public class Opcode implements Byteable {
   }
 
   public enum Type {
-    LDA,
-    ADD,
-    SUB,
+
+    NOP(0),
+    ADD(1),
+    SUB(2),
+    MUL(3),
+    DIV(4),
+    MOVE(7),
+    LDA(8),
+    LD1(9),
+    LD2(10),
+    LD3(11),
+    LD4(12),
+    LD5(13),
+    LD6(14),
+    LDX(15),
+    LDAN(16),
+    LD1N(17),
+    LD2N(18),
+    LD3N(19),
+    LD4N(20),
+    LD5N(21),
+    LD6N(22),
+    LDXN(23),
+    STA(24),
+    ST1(25),
+    ST2(26),
+    ST3(27),
+    ST4(28),
+    ST5(29),
+    ST6(30),
+    STX(31),
+    STJ(32),
+    STZ(33),
+    JBUS(34),
+    IOC(35),
+    IN(36),
+    OUT(37),
+    JRED(38),
+    JMP(39),
+    JA(40),
+    J1(41),
+    J2(42),
+    J3(43),
+    J4(44),
+    J5(45),
+    J6(46),
+    JX(47);
+
+    private int mValue;
+    Type(int value) {
+      mValue = value;
+    }
   }
 
   public boolean equals(Object o) {
@@ -35,7 +84,7 @@ public class Opcode implements Byteable {
     }
     return true;
   }
-  
+
   public String toString() {
     return type.name();
   }
